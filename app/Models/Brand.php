dda52e02f1c62a7f420990b935d2dcf3c60ef810
Wagner Luis Resta Quirino Silveira <wagner@MacBook-Air-de-Wagner.local>
2025-10-23 16:44:15 -0300

    Backend para busca de Produtos

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -10,4 +10,9 @@ class Brand extends Model
     use HasFactory;
     
     protected $fillable = ['description'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
---
+++ app/Models/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'brand_id',
+        'category_id',
+        'price',
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function getPriceAttribute($value)
+    {
+        return number_format($value, 2, ',', '.');
+    }
+}
